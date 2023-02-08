69721ff40fdab06ce181b0c1a2d128c4c9fb0b5c
Vahe <vabardeg@gmail.com>
2023-02-08 14:17:37 +0400

    long type sales

--- sale/src/Main.java
+++ sale/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] sales = {45, 65, 76, 23, 876, 1231, 675, 978, 123};
+        long[] sales = {45, 65, 76, 23, 876, 1231, 675, 978, 123};
         SalesManager salesManager = new SalesManager(sales);
 
         System.out.println(salesManager.max());
