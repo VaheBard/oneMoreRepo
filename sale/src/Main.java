d258991ca4ff497cf6cea8c32afd1debe58c7624
Vahe <vabardeg@gmail.com>
2023-02-08 14:50:52 +0400

    creating method stat

--- sale/src/Main.java
+++ sale/src/Main.java
@@ -1,9 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] sales = {45, 65, 76, 23, 876, 1231, 675, 978, 123};
+        int[] sales = {4, 5, 6, 7, 8};
         SalesManager salesManager = new SalesManager(sales);
 
         System.out.println(salesManager.max());
+        System.out.println(average(sales));
+    }
+    public static int average(int [] array){
+        int sum = 0;
+        int min = 0;
+        int max = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            if(min <= array[i]){
+                min = array[i];
+            }
+            if(array[i] >array[i + 1]){
+                max = array[i];
+            }
+            sum += array[i];
+        }
+        int average = (sum - min - max) / array.length;
+        return average;
+
     }
 }
